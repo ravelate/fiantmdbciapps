798ca859735d1cbf2b8612d3d882ced65c303925
Fian Ardi Saputra <fianardi25@gmail.com>
2023-10-17 10:17:58 +0700

    update test

--- core/src/androidTest/java/com/felina/Fiantmdbmovie/core/ExampleInstrumentedTest.kt
+++ core/src/androidTest/java/com/felina/Fiantmdbmovie/core/ExampleInstrumentedTest.kt
@@ -20,6 +20,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dicoding.tourismapp.core.test", appContext.packageName)
+        assertEquals("com.felina.Fiantmdbmovie.core.test", appContext.packageName)
     }
 }
